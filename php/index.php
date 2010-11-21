903af0ba2f463ef6716843d08a59c2fcee650dcd
Rahul Garg <rahul@abstract.cs.washington.edu>
2010-11-21 09:42:17 -0800

    Nothing imp

--- php/index.php
+++ php/index.php
@@ -201,7 +201,8 @@ if ($session) {
         <img src="../resources/Button-Close-icon.png" 
             id="fullres_photo_close_button">
         </img>
-        <img id="fullres_photo_img"></img>
+        <img id="fullres_photo_img">
+        </img>
     </div>
   </div>
   <script>
